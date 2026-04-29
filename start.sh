33090cb3d28c7471710b06b3ee7dd5b6fe15253d
Hamidreza Dehghan <hamidd0740@gmail.com>
2026-04-29 04:16:58 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+mkdir -p /var/lib/telegram-bot-api
 exec telegram-bot-api \
   --api-id=$TELEGRAM_API_ID \
   --api-hash=$TELEGRAM_API_HASH \
   --http-port=8085 \
   --local \
-  --dir=/
+  --dir=/var/lib/telegram-bot-api
